2cb2ad48b69ce6d9d24b0ffa9cb3569cda172bc7
khushi <skhushi0218@gmial.com>
2025-07-18 19:55:14 +0545

    Initial clean commit without db.sqlite3

--- drink/settings.py
+++ drink/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
-
+ALLOWED_HOSTS = [ ]
 # Application definition
 
 INSTALLED_APPS = [
